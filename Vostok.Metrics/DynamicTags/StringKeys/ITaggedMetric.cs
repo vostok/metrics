b141008fa421b7f58602c660fff267572d6948e4
ezsilmar <zhirov@skbkontur.ru>
2019-03-19 12:22:03 +0500

    Added the first T4 template.. There're going to be lot of them

--- Vostok.Metrics/DynamicTags/StringKeys/ITaggedMetric.cs
+++ Vostok.Metrics/DynamicTags/StringKeys/ITaggedMetric.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 
 namespace Vostok.Metrics.DynamicTags.StringKeys
 {
@@ -6,14 +6,32 @@ namespace Vostok.Metrics.DynamicTags.StringKeys
     {
         TMetric For(string value1);
     }
-    
     public interface ITaggedMetric2<out TMetric> : IDisposable
     {
         TMetric For(string value1, string value2);
     }
-
     public interface ITaggedMetric3<out TMetric> : IDisposable
     {
         TMetric For(string value1, string value2, string value3);
+    }
+    public interface ITaggedMetric4<out TMetric> : IDisposable
+    {
+        TMetric For(string value1, string value2, string value3, string value4);
+    }
+    public interface ITaggedMetric5<out TMetric> : IDisposable
+    {
+        TMetric For(string value1, string value2, string value3, string value4, string value5);
+    }
+    public interface ITaggedMetric6<out TMetric> : IDisposable
+    {
+        TMetric For(string value1, string value2, string value3, string value4, string value5, string value6);
+    }
+    public interface ITaggedMetric7<out TMetric> : IDisposable
+    {
+        TMetric For(string value1, string value2, string value3, string value4, string value5, string value6, string value7);
+    }
+    public interface ITaggedMetric8<out TMetric> : IDisposable
+    {
+        TMetric For(string value1, string value2, string value3, string value4, string value5, string value6, string value7, string value8);
     }
 }
