93c7263b4d3cccfe4a51c1d81af34a2f9eabadb8
iloktionov <iloktionov@skbkontur.ru>
2019-03-23 21:01:09 +0500

    MetricEvent: reformat + xml-docs.

--- Vostok.Metrics/Model/MetricEvent.cs
+++ Vostok.Metrics/Model/MetricEvent.cs
@@ -1,82 +1,77 @@
 using System;
 using JetBrains.Annotations;
-using Vostok.Metrics.Primitives.CounterPrimitive;
 
 namespace Vostok.Metrics.Model
 {
     /// <summary>
-    /// <para>
-    /// Represents a single <see cref="Value"/> scraped from the metric primitive
-    /// and a user's intention (see <see cref="AggregationType"/>) to do something with this value.
-    /// </para> 
+    /// <para><see cref="MetricEvent"/> is the atomic storage unit of Vostok metrics system.</para>
+    /// <para>Every event contains a numeric <see cref="Value"/> measured at some <see cref="Timestamp"/> and bound to a set of <see cref="Tags"/>.</para>
+    /// <para>Events may also contain auxiliary information, such as <see cref="Unit"/> and <see cref="AggregationType"/>.</para>
+    /// <para><see cref="MetricEvent"/> instances are immutable. Consider using <see cref="MetricEventBuilder"/> to construct them.</para>
     /// </summary>
     [PublicAPI]
     public class MetricEvent
     {
         /// <summary>
-        /// The value of this MetricSample 
+        /// 
         /// </summary>
-        public double Value { get; }
-        
-        /// <summary>
-        /// When the <see cref="Value"/> was observed.
-        /// </summary>
-        public DateTimeOffset Timestamp { get; }
-        
-        /// <summary>
-        /// <para>The unit of the <see cref="Value"/>.</para>
-        /// <para>See well-known units in <see cref="WellKnownUnits"/></para>
-        /// </summary>
-        [CanBeNull]
-        [ValueProvider("Vostok.Metrics.WellKnownUnits")]
-        public string Unit { get; }
-        
-        /// <summary>
-        /// <para>AggregationType defines what will happen with the MetricSample.</para>
-        /// <para>
-        /// If AggregationType is <c>null</c> the sample should be saved as-is to a persistent storage like Graphite
-        /// </para>
-        /// <para>
-        /// Otherwise, the sample will undergo some aggregation process.
-        /// For example, samples produced by <see cref="ICounter"/> primitive
-        /// should be summed up server-side before saving.
-        /// </para>
-        /// <para>
-        /// See well-known aggregation types in <see cref="WellKnownAggregationTypes"/>
-        /// </para>
-        /// </summary>
-        [CanBeNull]
-        [ValueProvider("Vostok.Metrics.WellKnownAggregationTypes")]
-        public string AggregationType { get; }
-        
-        /// <summary>
-        /// <inheritdoc cref="MetricTags"/>
-        /// </summary>
-        public MetricTags Tags { get; }
-
-        /// <summary>
-        /// <inheritdoc cref="MetricEvent"/>
-        /// <para>You may also use <see cref="MetricEventBuilder"/> to create a MetricSample</para>
-        /// </summary>
-        /// <param name="value">Observed value</param>
-        /// <param name="tags">Collection of tags describing the value. See <see cref="Tags"/></param>
-        /// <param name="timestamp">When the value was observed</param>
-        /// <param name="unit">Unit of the value</param>
-        /// <param name="aggregationType">What to do with the value. See <see cref="AggregationType"/></param>
+        /// <param name="value">See <see cref="Value"/>.</param>
+        /// <param name="tags">See <see cref="Tags"/>.</param>
+        /// <param name="timestamp">See <see cref="Timestamp"/>.</param>
+        /// <param name="unit">See <see cref="Unit"/>.</param>
+        /// <param name="aggregationType">See <see cref="AggregationType"/>.</param>
         public MetricEvent(
             double value,
             MetricTags tags,
             DateTimeOffset timestamp,
-            [ValueProvider("Vostok.Metrics.WellKnownUnits")]
+            [CanBeNull] [ValueProvider("Vostok.Metrics.WellKnownUnits")]
             string unit,
-            [ValueProvider("Vostok.Metrics.WellKnownAggregationTypes")]
+            [CanBeNull] [ValueProvider("Vostok.Metrics.WellKnownAggregationTypes")]
             string aggregationType)
         {
+            Tags = tags ?? throw new ArgumentNullException(nameof(tags));
+
+            if (tags.Count == 0)
+                throw new ArgumentException("Empty tags are not allowed in metric events.", nameof(tags));
+
+            Unit = unit;
             Value = value;
             Timestamp = timestamp;
-            Unit = unit;
             AggregationType = aggregationType;
-            Tags = tags;
         }
+
+        /// <summary>
+        /// Numeric value of the metric represented by this <see cref="MetricEvent"/>.
+        /// </summary>
+        public double Value { get; }
+
+        /// <summary>
+        /// Timestamp denoting the moment when this <see cref="MetricEvent"/>'s <see cref="Value"/> was observed.
+        /// </summary>
+        public DateTimeOffset Timestamp { get; }
+
+        /// <summary>
+        /// <para>Set of key-value string tags this event's <see cref="Value"/> is bound to. May not be empty.</para>
+        /// <para>See <see cref="MetricTags"/> for more details.</para>
+        /// </summary>
+        [NotNull]
+        public MetricTags Tags { get; }
+
+        /// <summary>
+        /// <para>Unit of measurement this event's <see cref="Value"/> is expressed in.</para>
+        /// <para>Dimensionless quantities (such as count) may have no unit (<c>null</c> value).</para>
+        /// <para>See <see cref="WellKnownUnits"/> for predefined unit constants.</para>
+        /// </summary>
+        [CanBeNull]
+        public string Unit { get; }
+
+        /// <summary>
+        /// <para>Aggregation type defines what happens to the event after it's constructed and sent.</para>
+        /// <para><c>Null</c> value means no aggregation: event will be published to the metrics storage of choice (like Graphite or Prometheus) as is.</para>
+        /// <para>Any non-null value implies server-side processing: event will undergo some kind of aggregation that will eventually produce other events with <c>null</c> <see cref="AggregationType"/>.</para>
+        /// <para>See <see cref="WellKnownAggregationTypes"/> for predefined type constants.</para>
+        /// </summary>
+        [CanBeNull]
+        public string AggregationType { get; }
     }
 }
