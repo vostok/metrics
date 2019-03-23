e5f86a2016183042430082209b1d637f76776fb8
iloktionov <iloktionov@skbkontur.ru>
2019-03-23 19:36:02 +0500

    MetricTag: cleanup.

--- Vostok.Metrics/Model/MetricTag.cs
+++ Vostok.Metrics/Model/MetricTag.cs
@@ -1,26 +1,24 @@
+using System;
 using JetBrains.Annotations;
 
 namespace Vostok.Metrics.Model
 {
     /// <summary>
-    /// Represents a single item in <see cref="MetricTags"/> collection
+    /// Represents a single item in <see cref="MetricTags"/> collection.
     /// </summary>
     [PublicAPI]
     public class MetricTag
     {
+        public MetricTag([NotNull] string key, [NotNull] string value)
+        {
+            Key = key ?? throw new ArgumentNullException(nameof(key));
+            Value = value ?? throw new ArgumentNullException(nameof(value));
+        }
+
         [NotNull]
-        [ValueProvider("Vostok.Metrics.WellKnownTagKeys")]
         public string Key { get; }
-        
+
         [NotNull]
         public string Value { get; }
-
-        public MetricTag(
-            [NotNull] [ValueProvider("Vostok.Metrics.WellKnownTagKeys")] string key,
-            [NotNull] string value)
-        {
-            Key = key;
-            Value = value;
-        }
     }
 }
