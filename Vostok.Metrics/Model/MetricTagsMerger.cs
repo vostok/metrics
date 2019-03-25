b6bf6335dc6ac8723527432231affbed777e72c0
iloktionov <iloktionov@skbkontur.ru>
2019-03-25 12:43:42 +0500

    MetricTagsMerger: xml-docs.

--- Vostok.Metrics/Model/MetricTagsMerger.cs
+++ Vostok.Metrics/Model/MetricTagsMerger.cs
@@ -5,14 +5,23 @@ namespace Vostok.Metrics.Model
     [PublicAPI]
     public static class MetricTagsMerger
     {
-        public static MetricTags Merge(MetricTags contextTags, string name, MetricTags dynamicTags)
+        /// <summary>
+        /// <para>Appends a <see cref="WellKnownTagKeys.Name"/> tag with value taken from given <paramref name="name"/> parameter to given <paramref name="contextTags"/>.</para>
+        /// <para>Then appends <paramref name="dynamicTags"/> after <see cref="WellKnownTagKeys.Name"/> and returns the result.</para>
+        /// </summary>
+        [NotNull]
+        public static MetricTags Merge([NotNull] MetricTags contextTags, [NotNull] string name, [NotNull] MetricTags dynamicTags)
         {
             return contextTags
                 .Append(WellKnownTagKeys.Name, name)
                 .Append(dynamicTags);
         }
 
-        public static MetricTags Merge(MetricTags contextTags, string name)
+        /// <summary>
+        /// Appends a <see cref="WellKnownTagKeys.Name"/> tag with value taken from given <paramref name="name"/> parameter to given <paramref name="contextTags"/> and returns resulting tags.
+        /// </summary>
+        [NotNull]
+        public static MetricTags Merge([NotNull] MetricTags contextTags, [NotNull] string name)
         {
             return contextTags
                 .Append(WellKnownTagKeys.Name, name);
