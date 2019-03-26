618d38bd73ca910c1be59551dcd966cc8a99561f
iloktionov <iloktionov@skbkontur.ru>
2019-03-26 15:57:18 +0500

    + DevNullMetricContext

---
+++ Vostok.Metrics/DevNullMetricContext.cs
@@ -0,0 +1,29 @@
+using System;
+using JetBrains.Annotations;
+using Vostok.Metrics.Model;
+
+namespace Vostok.Metrics
+{
+    /// <summary>
+    /// A trivial implementation of <see cref="IMetricContext"/> that simply does nothing.
+    /// </summary>
+    [PublicAPI]
+    public class DevNullMetricContext : IMetricContext
+    {
+        public MetricTags Tags => MetricTags.Empty;
+
+        public IDisposable Register(IScrapableMetric metric, TimeSpan? scrapePeriod)
+            => new DummyRegistrationToken();
+
+        public void Send(MetricEvent @event)
+        {
+        }
+
+        private class DummyRegistrationToken : IDisposable
+        {
+            public void Dispose()
+            {
+            }
+        }
+    }
+}
