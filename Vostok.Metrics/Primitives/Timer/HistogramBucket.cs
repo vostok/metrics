017bcef9105f83b2fa0f5f2990ab5b3760b64d4c
kungurtsev <kungurtsev@skbkontur.ru>
2019-06-28 12:59:14 +0500

    HistogramBucket Equals

--- Vostok.Metrics/Primitives/Timer/HistogramBucket.cs
+++ Vostok.Metrics/Primitives/Timer/HistogramBucket.cs
@@ -17,5 +17,27 @@ namespace Vostok.Metrics.Primitives.Timer
             LowerBound = lowerBound;
             UpperBound = upperBound;
         }
+
+        #region Equality
+
+        public bool Equals(HistogramBucket other) =>
+            LowerBound.Equals(other.LowerBound) && UpperBound.Equals(other.UpperBound);
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj))
+                return false;
+            return obj is HistogramBucket other && Equals(other);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (LowerBound.GetHashCode() * 397) ^ UpperBound.GetHashCode();
+            }
+        }
     }
+
+    #endregion
 }
